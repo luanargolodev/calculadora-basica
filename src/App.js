41d2f37705ee9e97aa8ebbf8942224dfc6b77a2f
Luan Argolo <ipsluan@icloud.com>
2021-09-01 19:25:39 -0300

    :sparkles: Added footer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ const App = () => {
   const clear = () => {
     setResult("");
   };
-  const backspace = () => {};
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+  const value = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch(err) {
+      setResult("Error!");
+    }
+  }
   return (
     <>
       <div className="container">
@@ -17,26 +26,29 @@ const App = () => {
           <input type="text" value={result} />
         </form>
         <div className="keypad">
-          <button onClick={clear} id="clear">Clear</button>
-          <button onClick={backspace} id="backspace">C</button>
-          <button name="/" onClick={handleClick}>&divide;</button>
+          <button className="highlight" onClick={clear} id="clear">Apagar</button>
+          <button className="highlight" onClick={backspace} id="backspace">C</button>
+          <button className="highlight" name="/" onClick={handleClick}>&divide;</button>
           <button name="7" onClick={handleClick}>7</button>
           <button name="8" onClick={handleClick}>8</button>
           <button name="9" onClick={handleClick}>9</button>
-          <button name="*" onClick={handleClick}>&times;</button>
+          <button className="highlight" name="*" onClick={handleClick}>&times;</button>
           <button name="4" onClick={handleClick}>4</button>
           <button name="5" onClick={handleClick}>5</button>
           <button name="6" onClick={handleClick}>6</button>
-          <button name="-" onClick={handleClick}>&ndash;</button>
+          <button className="highlight" name="-" onClick={handleClick}>&ndash;</button>
           <button name="1" onClick={handleClick}>1</button>
           <button name="2" onClick={handleClick}>2</button>
           <button name="3" onClick={handleClick}>3</button>
-          <button name="+" onClick={handleClick}>+</button>
+          <button className="highlight" name="+" onClick={handleClick}>+</button>
           <button name="0" onClick={handleClick}>0</button>
           <button name="." onClick={handleClick}>.</button>
-          <button onClick={handleClick} id="result">=</button>
+          <button className="highlight" onClick={value} id="result">=</button>
         </div>
       </div>
+      <footer>
+        Feito por Luan Argolo
+      </footer>
     </>
   );
 };
