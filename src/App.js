7c10b41a0920591e15beeb7dcccd99e79e57ab3f
Luan Argolo <ipsluan@icloud.com>
2021-09-01 18:35:30 -0300

    :sparkles: Layout finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState("");
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button>Clear</button>
+          <button>C</button>
+          <button>&divide;</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>&times;</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>&ndash;</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
